eaf54c2c8aac85d4d1c990d04de310e22e8ccea3
Joao <joao.reboucas1@gmail.com>
2025-01-21 15:23:06 -0300

    Cleaning script for reusing

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # BibTex author shortener
 
 This is a simple Python script that shortens the authors field of your `BibTex` file.
+
+## Usage
+For help, run the command
+```
+	python3 bib_shorten.py --help
+```
--- bib_shorten.py
+++ bib_shorten.py
@@ -1,10 +1,14 @@
-file_name = "nls.bib"
-with open(file_name, "r") as f:
-    lines = f.readlines()
+import os
+from argparse import ArgumentParser
 
-def find_cut(string: str) -> str:
+argp = ArgumentParser()
+argp.add_argument("input",                           help="Input .bib file to shorten")
+argp.add_argument("-o",               required=True, help="Path to output the shortened bib file", metavar="output")
+argp.add_argument("--max-authors",    required=True, help="Maximum number of authors for each entry", type=int, metavar="N")
+argp.add_argument("--log",                           help="Whether to log the changes", action="store_true")
+
+def find_cut(string: str, max_authors: int) -> str:
     num_authors = 0
-    max_authors = 5
     for i, letter in enumerate(string):
         if letter == "a" and string[i-1].isspace() and string[i:i+3] == "and": 
             num_authors += 1
@@ -13,19 +17,28 @@ def find_cut(string: str) -> str:
                 elif string[0] == '{': close = '}'
                 return "      author = " + string[:i+3] + " others" + close + ",\n"
 
-count = 0
-new_lines = lines
-for i, line in enumerate(lines):
-    if "author" in line:
-        author, authors = line.strip().split("=")
-        num_authors = authors.count("and")
-        if num_authors > 5:
-            new_authors = find_cut(authors.strip())
-            print(f"{i} : {authors} \n=>\n{new_authors}")
-            new_lines[i] = new_authors
-            print("--------------")
-        count += 1
+if __name__ == "__main__":
+    args = argp.parse_args()
+    
+    if not os.path.isfile(args.input):
+        print(f"ERROR: could not open file {args.input}")
+        exit(1)
 
-output_file_name = "nls_short.bib"
-with open(output_file_name, "w") as f:
-    f.writelines(new_lines)
+    with open(args.input, "r") as f:
+        lines = f.readlines()
+
+    new_lines = lines
+    for i, line in enumerate(lines):
+        if "author" in line:
+            author, authors = line.strip().split("=")
+            num_authors = authors.count("and")
+            if num_authors > 5:
+                new_authors = find_cut(authors.strip(), max_authors=args.max_authors)
+                if args.log:
+                    print(f"{args.input}:{i+1}\n{line.strip()} \n>> Shortened to >>\n{new_authors.strip()}")
+                    print("--------------")
+                new_lines[i] = new_authors
+
+
+    with open(args.o, "w") as f:
+        f.writelines(new_lines)
